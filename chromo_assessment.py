c300e6cc4b8721d8089f0ba1d3afbca8df799f7b
Sarah Fumagalli <sefumagal@gmail.com>
2025-09-09 18:09:13 -0500

    updated sex chromos

--- chromo_assessment.py
+++ chromo_assessment.py
@@ -118,6 +118,90 @@ def add_chromos(mashmap, num_chromos, translation):
             translation.loc[len(translation)] = [addition[0], addition[5], addition[1], addition[6]]
 
         return translation
+    
+
+    
+    def sex_chr_check(j, i, unique_contigs, mashmap_sub, translation, current_haps, contig_exists):
+        """
+        
+        Check sex chromosome for length and PAR
+
+        """
+        print('unique_contigs=',unique_contigs) 
+        #if more than one contig, check lengths and for PAR
+        if len(unique_contigs) > 1:
+            print('more than one contig')
+            temp_df = mashmap_sub.copy()
+            print('temp_mashmap=',temp_df)
+            temp_df = temp_df[temp_df.iloc[:,0].apply(lambda x:i in x)]
+
+            if 'X' in j:
+                #check if any contigs are longer than 1000000 and start before 6000000 (PAR)
+                if (temp_df[10] >= 1000000).any():          #and (temp_df[10] > temp_df[10] - 6000000).any():
+                    print('X contig greater than 1mb and not PAR')
+                    if contig_exists:
+                        print('contig exists')
+                    else:
+                        translation = chromo_to_translation(temp_df, translation)
+                        current_haps.append(i)
+                    reorder_df = False
+                else:
+                    print('X contig less than 1mb - ignoring or PAR')
+                    if contig_exists:
+                        print('dropping PAR related chromosome')
+                        translation.drop(translation[translation[1].str.contains(j)].index, inplace=True)
+                    reorder_df = False
+
+            if 'Y' in j:
+                #check if any contigs are longer than 1000000 and start after 7000000 (PAR)
+                if (temp_df[10] >= 1000000).any() and (temp_df[7] > 7000000).any():
+                        print('Y contig greater than 1mb and not PAR')
+                        if contig_exists:
+                            print('contig exists')
+                        else:
+                            translation = chromo_to_translation(temp_df, translation)
+                            current_haps.append(i)
+                        reorder_df = True
+                else:
+                    print('Y contig less than 1mb - ignoring or PAR')
+                    if contig_exists:
+                        print('dropping PAR related chromosome')
+                        translation.drop(translation[translation[1].str.contains(j)].index, inplace=True)
+                    reorder_df = True
+        else:
+            print('only one contig')    
+            if (mashmap_sub[10] >= 1000000).any():
+                print('contig greater than 1mb')
+                if contig_exists:
+                    print('contig exists')
+                    #check to see if chromosome exists
+                    if translation[1].str.contains(j).any():
+                        print('contig really does exist')
+                    else:
+                        #add chromosome
+                        translation = chromo_to_translation(mashmap_sub, translation)
+                        current_haps.append(i)
+                else:
+                    print('add contig')
+                    translation = chromo_to_translation(mashmap_sub, translation)
+                    current_haps.append(i)
+                if 'Y' in j:
+                    reorder_df = True
+                else:
+                    reorder_df = False
+            else:
+                print('contig less than 1mb - ignoring')
+                if contig_exists:
+                    print('dropping PAR related contig')
+                    translation.drop(translation[translation[1].str.contains(j)].index, inplace=True)
+                if 'Y' in j:
+                    reorder_df = True
+                else:
+                    reorder_df = False
+
+
+        return translation, reorder_df
+
 
 
     #create expected chromosome list for reference
@@ -133,15 +217,12 @@ def add_chromos(mashmap, num_chromos, translation):
     #iterate through expected chromosome list
     for index, j in enumerate(chromo_list):
         print('chromo=',j)
-        
-        mashmap_copy = mashmap.copy()
-        
         #filter mashmap for chromosome
         mashmap_sub = mashmap_copy[mashmap_copy.iloc[:,5].apply(lambda x:j in x)] 
         if mashmap_sub.empty:
             print('chromosome not found in mashmap')
             if j == 'chr_Y':
-                condition = True
+                reorder_df = True
             continue
 
         #sort by alignment length
@@ -155,48 +236,22 @@ def add_chromos(mashmap, num_chromos, translation):
             #print('unique contig=',i)
             if i in current_haps:
                 print('chromosome already exists in translation file')
+                #this may be incorrect if there is a duplicate contig name (happens more often for sex chromosomes)
+                contig_exists = True
+                #double check verkko-fillet's decision 
+                if j == 'chr_X' or j == 'chr_Y':
+                    print('double checking sex chromosomes')
+                    translation, reorder_df = sex_chr_check(j, i, unique_contigs, mashmap_temp, translation, current_haps, contig_exists)
                 if j == 'chr_Y':
-                    condition = True
-                break
+                    reorder_df = True
+                    break
             else:
                 #check sex chromosomes 
+                contig_exists = False
                 if 'X' in j or 'Y' in j:
                     print('X or Y contig')
-
-                    #if more than one contig, check lengths and for PAR
-                    if len(unique_contigs) > 1:
-                        print('more than one contig')
-                        temp_df = mashmap_sub.copy()
-                        temp_df = mashmap_sub[mashmap_sub.iloc[:,0].apply(lambda x:i in x)]
-                        if 'X' in j:
-                            #check if any contigs are longer than 1000000 and less than 6000000
-                            if (temp_df[10] >= 1000000).any() and (temp_df[7] < temp_df[7] - 6000000).any():
-                                print('X contig greater than 1mb and not PAR')
-                                translation = chromo_to_translation(temp_df, translation)    
-                                current_haps.append(i)
-                            else:
-                                print('X contig less than 1mb - ignoring or PAR')
-                        if 'Y' in j:
-                            #check if any contigs are longer than 1000000 and greater than 7000000
-                            if (temp_df[10] >= 1000000).any() and (temp_df[7] > 7000000).any():
-                                print('Y contig greater than 1mb and not PAR')
-                                translation = chromo_to_translation(temp_df, translation)
-                                current_haps.append(i)
-                            else:
-                                print('Y contig less than 1mb - ignoring or PAR')
-                                condition = True
-                                break
-                    else:
-                        print('only one contig')
-                        if (mashmap_sub[10] >= 1000000).any():
-                            print('contig greater than 1mb')
-                            translation = chromo_to_translation(mashmap_sub, translation)   
-                            current_haps.append(i)
-                        else:
-                            print('contig less than 1mb - ignoring')
-                            if 'Y' in j:
-                                condition = True
-                else:
+                    translation, reorder_df = sex_chr_check(j, i, unique_contigs, mashmap_temp, translation, current_haps, contig_exists)             
+               else:
                     #check non-sex chromosomes
                     if len(unique_contigs) > 1:
                         print('more than one contig')
@@ -210,13 +265,16 @@ def add_chromos(mashmap, num_chromos, translation):
                         else:
                             print('contig less than 1mb - ignoring')
                             if 'Y' in j: 
-                                condition = True
+                                reorder_df = True
                     else:
                         print('only one contig')
                         translation = chromo_to_translation(mashmap_sub, translation)
                         current_haps.append(i)
             
+
     return translation, condition
+
+
 
 
 
@@ -282,15 +340,15 @@ def find_sex_chromos(mashmap, translation_hap1, translation_hap2):
         print('chromosome X already exists in translation files')
     else:
         #check if any contigs are longer than 1000000 and less than 6000000
-        if (chr_X[10] >= 1000000).any() and (chr_X[10] > chr_X[10] - 6000000).any():
+        if (chr_X[10] >= 1000000).any(): # and (chr_X[10] > chr_X[10] - 6000000).any():
             print('X contig greater than 1mb and not PAR')
             addition = chr_X.values.tolist()[0]
             if 'haplotype1' in addition[0]:
                 print('chr_X added to dam')
-                translation_hap2.loc[len(translation_hap2)] = [addition[0], addition[5], addition[1], addition[6]]
+                translation_hap1.loc[len(translation_hap1)] = [addition[0], addition[5], addition[1], addition[6]]
             else:
                 print('chr_X added to sire')
-                translation_hap1.loc[len(translation_hap1)] = [addition[0], addition[5], addition[1], addition[6]]
+                translation_hap2.loc[len(translation_hap2)] = [addition[0], addition[5], addition[1], addition[6]]
         else:
             print('X contig less than 1mb - ignoring or PAR')
 
@@ -303,7 +361,7 @@ def find_sex_chromos(mashmap, translation_hap1, translation_hap2):
             print('Y contig greater than 1mb and not PAR')
             addition = chr_Y.values.tolist()[0]
             print('added chr_Y to sire')
-            translation_hap1.loc[len(translation_hap1)] = [addition[0], addition[5], addition[1], addition[6]]
+            translation_hap2.loc[len(translation_hap2)] = [addition[0], addition[5], addition[1], addition[6]]
         else:
             print('Y contig less than 1mb - ignoring or PAR')
 
@@ -311,22 +369,19 @@ def find_sex_chromos(mashmap, translation_hap1, translation_hap2):
     return translation_hap1, translation_hap2
 
 
-
-
-#initial 'condition' is set to false -- df will not be reordered
-condition = False
+#initial 'reorder_df' is set to false -- df will not be reordered
+reorder_df = False
 
 #iterate through each translation file
 for j,i in enumerate(trans_files):
     print(i)
-    
     #check if translation file is empty
     file_size = os.path.getsize(i)
     if file_size > 0:
         translation = pd.read_csv(i, sep='\t', header=None)
     else:
         translation = pd.DataFrame()
-    print('trans1=',translation) 
+    print('trans=',translation) 
 
     #if phasing data is trio and hic, mashmap has been previously filtered
     if trio_hic:
@@ -335,7 +390,6 @@ for j,i in enumerate(trans_files):
         except EmptyDataError:
             print("Mashmap haplotype file is empty. May need to # out mashmap_hap1 and mashmap_hap2 lines of code in sh file")
     else:
-        #grab mashmap for assemblies using trio or hic
         try:
             mashmap = pd.read_csv(mashmap_files[j], sep='\t', header=None)
         except EmptyDataError:
@@ -349,34 +403,41 @@ for j,i in enumerate(trans_files):
         print('find missing chromosomes')
         needed_chromos = missing_chromos(translation, num_chromos)
     
-        if len(needed_chromos) == 0:
-            print('all chromosomes already listed')
-            translation = reorder(translation, num_chromos)
-            if j == 0:
-                translation_hap1 = translation
-            else:
-                translation_hap2 = translation
-        else:
-            print('add new chromosomes to translation file')
-            translation, condition = add_chromos(mashmap, num_chromos, translation)
-            if condition: 
-                translation = reorder(translation, num_chromos)       
+        print('add new chromosomes to translation file')
+        mashmap_sub = mashmap.copy()
+        translation, reorder_df = add_chromos(mashmap_sub, num_chromos, translation)
+        if reorder_df: 
+            translation = reorder(translation, num_chromos)       
                               
-            #check for duplicate contigs
-            duplicate_contigs = translation[translation.duplicated([0])]
-            if len(duplicate_contigs) > 0:
-               print('warning: duplicate contig names')
-               print('duplicate_contigs=',duplicate_contigs)
-               translation = reorder(translation, num_chromos)
-            
-            #rename files
-            if j == 0:
-                translation_hap1 = translation
+        #check for duplicate contigs
+        duplicate_contigs = translation[translation.duplicated([0])]
+        if len(duplicate_contigs) > 0:
+            print('warning: duplicate contig names')
+            print('duplicate_contigs=',duplicate_contigs)
+            translation = reorder(translation, num_chromos)
+
+            #check duplicates for sex chromosomes   
+            if duplicate_contigs[1].str.contains('_chr_X').any() or duplicate_contigs[1].str.contains('_chr_Y').any():
+                if j == 0:
+                    print('hap1 sex chr duplicates found')
+                    hap1_dups = duplicate_contigs[duplicate_contigs[1].str.contains('|'.join(['_chr_X', '_chr_Y']))]
+                else:
+                    print('hap2 sex chr duplicates found')
+                    hap2_dups = duplicate_contigs[duplicate_contigs[1].str.contains('|'.join(['_chr_X', '_chr_Y']))]
             else:
-                translation_hap2 = translation
+                if j == 0:
+                    hap1_dups = pd.DataFrame()
+                else:
+                    hap2_dups = pd.DataFrame()
+                
+        if j == 0:
+            hap1_dups = pd.DataFrame()
+            translation_hap1 = translation
+        else:
+            hap2_dups = pd.DataFrame()
+            translation_hap2 = translation
             
     
-#if phasing data used in assembly is trio and hic, double check for haplotype named sex chromosomes
 if trio_hic:
     print('check haplotype files for missing chromosomes')
     mashmap1 = pd.read_csv(mashmap_files2[0], sep='\t', header=None)
@@ -384,11 +445,60 @@ if trio_hic:
     mashmap = pd.concat([mashmap1, mashmap2], axis=0)
        
     translation_hap1, translation_hap2 = find_sex_chromos(mashmap, translation_hap1, translation_hap2)
-        
+    
 
-#save translation files
+
+def remove_sex_duplicates(df, dam_sire):
+    """
+
+    Removes X for sire and Y for dam
+
+    """
+    
+    if dam_sire:
+        #check sire df for X chr
+        if df[1].str.contains('_chr_X').any() and df[0].str.contains('sire').any():
+            print('drop chr X from sire')
+            df.drop(df[df[1].str.contains('_chr_X')].index, inplace=True)
+
+        #check dam df for Y chr
+        if df[1].str.contains('_chr_Y').any() and df[0].str.contains('dam').any():
+            print('drop chr Y from dam')
+            df.drop(df[df[1].str.contains('_chr_Y')].index, inplace=True)
+    else:
+        #check haplotype df for X
+        print('drop chr X from haplotype')
+        df.drop(df[df[1].str.contains('_chr_X')].index, inplace=True)
+
+    return df
+
+
+
+
+print('checking sex chromosome duplicates')
+if translation_hap2[0].str.contains('sire').any() and translation_hap1[0].str.contains('dam').any():
+    print('dam and sire')
+    dam_sire = True
+    if len(hap1_dups) > 0:
+        print('hap1 duplicates=', hap1_dups)
+        translation_hap1 = remove_sex_duplicates(translation_hap1, dam_sire)
+    if len(hap2_dups) > 0:
+        print('hap2 duplicates=', hap2_dups)
+        translation_hap2 = remove_sex_duplicates(translation_hap2, dam_sire)
+else:
+    print('hap1 and hap2')
+    dam_sire = False
+    #check for chr Y
+    if translation_hap1[1].str.contains('_chr_Y').any():
+        print('hap1 contains chr Y')
+        translation_hap1 = remove_sex_duplicates(translation_hap1, dam_sire)
+    if translation_hap2[1].str.contains('_chr_Y').any():
+        print('hap2 contains chr Y')
+        translation_hap2 = remove_sex_duplicates(translation_hap2, dam_sire)
+
+
 translation_hap1.to_csv('translation_hap1.csv', sep="\t", header=False, index=False)
 translation_hap2.to_csv('translation_hap2.csv', sep="\t", header=False, index=False)
 
-    
+
 
